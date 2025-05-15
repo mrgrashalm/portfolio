943a40ddec3775eaba7be9e9c4114323f6377044
mrgrashalm <mr.grashalm@web.de>
2025-05-16 01:23:58 +0200

    Add timeline and edit landing and splash

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { SplashScreenComponent } from './splash-screen/splash-screen.component';
 import { LandingComponent } from './landing/landing.component';
 import { ColorsComponent } from './colors/colors.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SplashScreenComponent,
     LandingComponent,
-    ColorsComponent
+    ColorsComponent,
+    TimelineComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
--- src/app/splash-screen/splash-screen.component.ts
+++ src/app/splash-screen/splash-screen.component.ts
@@ -17,7 +17,7 @@ export class SplashScreenComponent implements OnInit {
       setTimeout(() => {
         this.showSplash = !this.showSplash;
       }, 500);
-    }, 3000);
+    }, 2500);
   }
 
 }
