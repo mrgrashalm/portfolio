ba43f0488f3745c72cf649a6ae0da9faafb52571
mrgrashalm <mr.grashalm@web.de>
2025-05-22 01:24:16 +0200

    Add styling to punk-de

--- src/app/my-projects/punk-de/punk-de.component.ts
+++ src/app/my-projects/punk-de/punk-de.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import lgZoom from 'lightgallery/plugins/zoom';
+import { BeforeSlideDetail } from 'lightgallery/lg-events';
 
 @Component({
   selector: 'app-punk-de',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class PunkDeComponent {
 
+    settings = {
+        counter: false,
+        plugins: [lgZoom]
+    };
+    onBeforeSlide = (detail: BeforeSlideDetail): void => {
+        const { index, prevIndex } = detail;
+        console.log(index, prevIndex);
+    };
+
+
+    constructor() { }
+
+    ngOnInit(): void {
+        document.documentElement.style.setProperty('--primary', '#B41B6D');
+        document.documentElement.style.setProperty('--font-family', 'Jersey25, sans-serif');
+    }
+
+    // Add any additional methods or properties you need for this component
+
+    ngOnDestroy(): void {
+        document.documentElement.style.setProperty('--primary', '#FF8C3A');
+        document.documentElement.style.setProperty('--font-family', 'Arial, Helvetica, sans-serif');
+    }
 }
